6ed59fa19ca132c1d74bcf07eb82a4bd0aef861a
Nawaf AlKhaldi <H-2w@hotmail.com>
2022-09-21 22:30:12 +0300

    Add project files.

---
+++ Athan/Form1.Designer.vb
@@ -0,0 +1,81 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "Athan"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Button2
+        '
+        Me.Button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(14, Byte), Integer), CType(CType(23, Byte), Integer), CType(CType(48, Byte), Integer))
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.ForeColor = System.Drawing.Color.White
+        Me.Button2.Location = New System.Drawing.Point(6, 5)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(97, 27)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "End"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(14, Byte), Integer), CType(CType(23, Byte), Integer), CType(CType(48, Byte), Integer))
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(6, 36)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(97, 27)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Hide"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(14, Byte), Integer), CType(CType(23, Byte), Integer), CType(CType(48, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(108, 72)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Opacity = 0.75R
+        Me.Text = "Athkar"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Athan/Form1.vb
@@ -0,0 +1,130 @@
+﻿Imports System.ComponentModel
+Imports System.Text.RegularExpressions
+Imports System.Threading
+
+Public Class Form1
+    Dim times As New List(Of String)
+    Dim nextPraytime
+    Dim notified As Boolean = False
+    Dim url As String = "https://www.islamicfinder.org/prayer-widget/110336/shafi/4/0/18.5/10"
+    Dim pos As Point
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Size = New Size(112, 72)
+        Control.CheckForIllegalCrossThreadCalls = False
+        Dim thread As New Thread(AddressOf getTimes) : thread.Start()
+        Dim thread2 As New Thread(AddressOf _loop) : thread2.Start()
+
+        Me.Close()
+    End Sub
+    Private Sub Form1_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            Me.Location += Control.MousePosition - pos
+        End If
+        pos = Control.MousePosition
+    End Sub
+    Sub getTimes()
+        Do
+            Try
+                'Clear Existing times
+
+                times.Clear()
+
+                Dim wc = New Net.WebClient
+
+                'Get Prayer times
+                Dim response As String = wc.DownloadString(url)
+                Dim matches As MatchCollection = Regex.Matches(response, "<p>(.*?)</p>")
+                For Each m As Match In matches
+                    Dim matched = m.Groups.Item(1).Value
+                    times.Add(matched)
+                Next
+                times.Remove(times(1))
+                Thread.Sleep(TimeSpan.FromHours(8))
+            Catch ex As Exception
+            End Try
+
+        Loop
+    End Sub
+    Sub _loop()
+        While True
+            nextprayer()
+            Thread.Sleep(TimeSpan.FromSeconds(5))
+        End While
+    End Sub
+    Sub nextprayer()
+        Try
+            For i = 0 To times.Count - 1
+                Dim curr As Date = Date.Now
+                Dim prayerTime As New Date(curr.Year, curr.Month, curr.Day, CDate(times(i)).Hour, CDate(times(i)).Minute, 0)
+                If (curr <= prayerTime) Then
+                    nextPraytime = prayerTime
+                    Me.Text = "Next: " + prayerTime.ToShortTimeString
+                    TimeChecker()
+                    Exit Sub
+                End If
+            Next
+        Catch ex As Exception
+        End Try
+    End Sub
+    Sub TimeChecker()
+        Dim curr As Date = Date.Now
+        Dim prayerTime As New Date(curr.Year, curr.Month, curr.Day, CDate(nextPraytime).Hour, CDate(nextPraytime).Minute, 0)
+        If (curr <= prayerTime.AddSeconds(-10)) Then
+            'not due yet
+
+            notified = False
+        Else
+            'due in 10 seconds
+
+            If Not notified Then
+                NotifyIcon1.Visible = True
+                NotifyIcon1.BalloonTipIcon = ToolTipIcon.Info
+                NotifyIcon1.BalloonTipTitle = "Athan"
+                For i = 0 To times.Count - 1
+                    If CDate(times(i)) = prayerTime.ToString("hh:mm tt") Then
+                        If i = 0 Then
+                            NotifyIcon1.BalloonTipText = $"أذان الفجر"
+                        ElseIf i = 1 Then
+                            NotifyIcon1.BalloonTipText = "أذان الظهر"
+                        ElseIf i = 2 Then
+                            NotifyIcon1.BalloonTipText = "أذان العصر"
+                        ElseIf i = 3 Then
+                            NotifyIcon1.BalloonTipText = "أذان المغرب"
+                        ElseIf i = 4 Then
+                            NotifyIcon1.BalloonTipText = "أذان العشاء"
+                        End If
+                        NotifyIcon1.ShowBalloonTip(50000)
+                    End If
+                Next
+                notified = True
+            End If
+        End If
+    End Sub
+#Region "Form1 Close/Hide Events"
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        'Hides app in background and show a Notification
+        e.Cancel = True
+        NotifyIcon1.Visible = True
+        NotifyIcon1.BalloonTipIcon = ToolTipIcon.Info
+        NotifyIcon1.BalloonTipTitle = "Application is running in background"
+        NotifyIcon1.BalloonTipText = "Click the Application icon to open the App"
+        NotifyIcon1.ShowBalloonTip(50000)
+        Me.Hide()
+        ShowInTaskbar = False
+    End Sub
+    Private Sub NotifyIcon1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles NotifyIcon1.Click
+
+        Me.Show()
+        ShowInTaskbar = True
+        NotifyIcon1.Visible = False
+    End Sub
+
+#End Region
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+    End Sub
+End Class
